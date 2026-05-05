fcc4ac7dfd825584cccdeec177306b84d92b85e2
Danielle Madrid <danielle.madrid@edu.pucrs.br>
2026-05-05 00:52:22 +0000

    feat/t1-Adicionando novos endppoints para jogos e contrato

--- demo/src/main/java/br/pucrs/daniellemadrid/demo/Controller.java
+++ demo/src/main/java/br/pucrs/daniellemadrid/demo/Controller.java
@@ -5,22 +5,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/acmespiele")
 public class Controller{
      private List<Cliente> clientes;
+     private List<Jogo> jogos;
+     private List<Contrato> contratos;
 
     public Controller(){
    clientes = new ArrayList<>(List.of(
                 new Cliente("838737473", "Maria", "maria@gmail.com", "usermaria", "password", java.sql.Date.valueOf("2000-09-11")),
                 new Cliente("234456778", "Geovana", "geo@gmail.com", "usergeo", "password", java.sql.Date.valueOf("1992-12-23")),
                 new Cliente("09837626", "Alice", "Alice@gmail.com", "userAli", "password", java.sql.Date.valueOf("2010-04-02"))));
-            }
+            
+    jogos = new ArrayList<>(List.of(
+                new Jogo(1, 2019, "The Last of Us Part II", 0.50),
+                new Jogo(2, 2020, "Cyberpunk 2077", 0.75),
+                new Jogo(3, 2026, "Resident Evil 9", 0.60)));  
+
+    contratos = new ArrayList<>(List.of(
+                new Contrato(1, 1, java.sql.Date.valueOf("2024-06-01")),
+                new Contrato(2, 2, java.sql.Date.valueOf("2024-07-01")),
+                new Contrato(3, 3, java.sql.Date.valueOf("2024-08-01"))));
+
+    }
 
  @GetMapping("/listaclientes")
     public List<Cliente> getListaClientes() {
         return clientes;
+    }
+
+  @GetMapping("/listajogos")
+  public List<Jogo> getListaJogos(){
+    return jogos;
+  }
+
+  @GetMapping("/listacontratos")
+  public List<Contrato> getListContratos(){
+    return contratos;
+  }
+
+ 
+  @GetMapping("/consultarjogossituacao/{situacao}")
+  public List<Jogo> getJogoPorSituacao(@PathVariable String situacao){
+    List<Jogo> jogoSituacao = new ArrayList<>();
+    for(Jogo jogo : jogos){
+      if(jogo.getSituacao().equals(situacao)){
+        jogoSituacao.add(jogo);
+      }
+    
+    return jogoSituacao;
+ }
+
+// A FINALIZAR (dani
+//Corpo da requisição {id, data, período, cpf, codigo}
+//resposta Retorna se o cadastro teve sucesso Booleano: true ou false )
+
+  @PostMapping("/cadastro/cadcontrato")
+    public Contrato cadastrarContrato(@RequestBody Contrato contrato){
+        contratos.add(contrato);
+        return contrato;
     }
 }
--- demo/src/main/java/br/pucrs/daniellemadrid/demo/Jogo.java
+++ demo/src/main/java/br/pucrs/daniellemadrid/demo/Jogo.java
@@ -12,7 +12,6 @@ public class Jogo {
         this.valorMinuto = valorMinuto;
     }
 
-
     public int getCodigo() {
         return this.codigo;
     }
@@ -43,5 +42,22 @@ public class Jogo {
 
     public void setValorMinuto(double valorMinuto) {
         this.valorMinuto = valorMinuto;
+    }
+
+    // A FINALIZAR (dani)
+    // Todo jogo inicialmente é cadastrado no sistema como disponível. Torna-se
+    // contratado se algum cliente realizar um contrato. 
+    // Torna-se obsoleto se o último contrato expirado foi há mais de 2 anos ou se não teve nenhum contrato após 2 anos de seu ano de lançamento.
+    // Um jogo é removido do sistema após 1 de se tornar obsoleto.
+    public String getSituacao(){
+        if(){
+      return "disponivel";
+       } else if(){
+           return "contratado";
+           } else if(){
+            return "obsoleto";
+            } else {
+           return "removido";
+        }
     }
 }
